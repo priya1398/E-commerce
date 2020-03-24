7faf72e9ff460104f940291e8643b6a1966ef1b2
salva-u <48282667+salva-u@users.noreply.github.com>
2020-03-24 20:16:47 +0400

    Churn model demo application
    Open this model using Pycharm, the main application code in is churnmodel folder and the virtual env is stored in the DTmodel

---
+++ Churn model application/churnmodel/app.py
@@ -0,0 +1,14 @@
+from flask import request
+from flask import jsonify
+from flask import Flask, render_template
+
+app =Flask (__name__)
+
+@app.route('/predict', methods=['POST'])
+
+def hello():
+    return render_template('predict.html')
+
+if __name__ is '__main__':
+    app.debug = True;
+    app.run()
---
+++ Churn model application/churnmodel/load.py
@@ -0,0 +1,15 @@
+import joblib
+
+def predict(dataset):
+    replace = {'VERY BAD':1, 'BAD':2, 'GOOD':3, 'VERY GOOD':4, 'LOW':1, 'MEDIUM':2, 'HIGH':3}
+    dataset = [replace[i] if i in replace else i for i in dataset]
+    dt = joblib.load('dt.model')
+    result = dt.predict([dataset])
+    if result == 1:
+        return True
+    else:
+        return False
+    return result
+
+a = predict(['GOOD', 'GOOD', 'GOOD', 'GOOD', 'MEDIUM', 'GOOD', 'GOOD'])
+print(a)
